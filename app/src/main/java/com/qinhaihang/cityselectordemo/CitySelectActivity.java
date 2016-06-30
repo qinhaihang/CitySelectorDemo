cd2e874bbf033c2dc992ac2ccff5c46b4d2f5b92
qinhaihang <hai449769034@qq.com>
2016-06-30 20:49:11 +0800

    6月30号 添加网络框架（未完善）

--- app/src/main/java/com/qinhaihang/cityselectordemo/CitySelectActivity.java
+++ app/src/main/java/com/qinhaihang/cityselectordemo/CitySelectActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
 
+import com.qinhaihang.cityselectordemo.Utils.NetURl;
+import com.qinhaihang.cityselectordemo.Utils.OkHttpClientManager;
+import com.squareup.okhttp.Request;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -38,6 +42,26 @@ public class CitySelectActivity extends AppCompatActivity {
     public void back(View view){
         finish();
         overridePendingTransition(0,R.anim.city_out_anim);
+    }
+
+    public void request(){
+        OkHttpClientManager.postAsyn(NetURl.getCityDataURL,
+                new OkHttpClientManager.ResultCallback() {
+                    @Override
+                    public void onError(Request request, Exception e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Object response) {
+
+                    }
+                },
+                new OkHttpClientManager.Param[]{
+                        new OkHttpClientManager.Param("uuid",""),
+                        new OkHttpClientManager.Param("SupCity",""),
+                        new OkHttpClientManager.Param("CityLev","")
+                });
     }
 
 }
---
+++ app/src/main/java/com/qinhaihang/cityselectordemo/Utils/NetURl.java
@@ -0,0 +1,12 @@
+package com.qinhaihang.cityselectordemo.Utils;
+
+/**
+ * Created by qinhh on 16/6/30.
+ */
+public class NetURl {
+    private static final String host = "http://60.205.57.113:8080/";         //linux测试
+    public static final String webService = host + "Bluesky365WebService/";
+
+    public static final String getCityDataURL = webService + "getCityData.do";
+
+}
